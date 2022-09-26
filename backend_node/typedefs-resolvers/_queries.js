e7602a983f9a900f310de629e0e97d02b7ddce6d
HRPzz <royalpkgh@naver.com>
2022-09-26 23:21:33 +0900

    feat: Use aruments and input type
    인자와 인풋 타입

--- backend_node/typedefs-resolvers/_queries.js
+++ backend_node/typedefs-resolvers/_queries.js
@@ -2,7 +2,17 @@ const { gql } = require('apollo-server')
 
 const typeDefs = gql`
     type Query {
-        people: [People],
+        people: [People]
+        peopleFiltered( # 여러 인자를 가짐
+            team: Int
+            sex: Sex
+            blood_type: BloodType
+            from: String
+        ): [People]
+        peoplePaginated( # 여러 인자를 가짐
+            page: Int!,
+            per_page: Int!
+        ): [People]
         equipments: [Equipment]
         equipmentAdvs: [EquipmentAdv]
         softwares: [Software]
--- backend_node/typedefs-resolvers/people.js
+++ backend_node/typedefs-resolvers/people.js
@@ -14,10 +14,27 @@ const typeDefs = gql`
         tools: [Tool] # interface
         givens: [Given] # union
     }
+
+    # Input Type - 넣어야 할 인자가 많을 경우 사용
+    input PostPersonInput {
+        first_name: String!
+        last_name: String!
+        sex: Sex!
+        blood_type: BloodType!
+        serve_years: Int!
+        role: Role!
+        team: ID!
+        from: String!
+    }
 `
 const resolvers = {
     Query: {
         people: (parent, args) => dbWorks.getPeople(args),
+        peopleFiltered: (parent, args) => dbWorks.getPeople(args),
+        peoplePaginated: (parent, args) => dbWorks.getPeople(args),
+    },
+    Mutation: {
+        postPerson: (parent, args) => dbWorks.postPerson(args),
     }
 }
 module.exports = {
